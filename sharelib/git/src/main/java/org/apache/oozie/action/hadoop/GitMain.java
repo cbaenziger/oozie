b502faa3132c569eef96ca0a25b9077e3f9650c0
Clay Baenziger <cbaenziger@bloomberg.net>
2017-05-01 21:37:20 -0400

    OOZIE-2877 Oozie Git Action

---
+++ sharelib/git/src/main/java/org/apache/oozie/action/hadoop/GitMain.java
@@ -0,0 +1,349 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.oozie.action.hadoop;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.oozie.action.hadoop.LauncherMain;
+
+import org.apache.oozie.action.hadoop.GitActionExecutor;
+import org.apache.oozie.util.XLog;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.TransportConfigCallback;
+import org.eclipse.jgit.transport.*;
+import org.eclipse.jgit.util.FS;
+
+
+
+public class GitMain extends LauncherMain {
+
+    private static final String HADOOP_USER = "user.name";
+    private static final String HADOOP_JOB_TRACKER = "mapred.job.tracker";
+    private static final String HADOOP_JOB_TRACKER_2 = "mapreduce.jobtracker.address";
+    private static final String HADOOP_YARN_RM = "yarn.resourcemanager.address";
+
+    // Private configuration variables
+    private String appName;
+    private String workflowId;
+    private String callbackUrl;
+    private String jobTracker;
+    private String nameNode;
+    private String keyPath;
+    private String destinationUri;
+    private String gitUri;
+    private String gitBranch;
+    private String actionType;
+    private String actionName;
+
+    private static final Set<String> DISALLOWED_PROPERTIES = new HashSet<String>();
+
+    static {
+        DISALLOWED_PROPERTIES.add(HADOOP_USER);
+        DISALLOWED_PROPERTIES.add(HADOOP_JOB_TRACKER);
+        DISALLOWED_PROPERTIES.add(HADOOP_JOB_TRACKER_2);
+        DISALLOWED_PROPERTIES.add(HADOOP_YARN_RM);
+    }
+
+    protected XLog LOG = XLog.getLog(getClass());
+
+    public static void main(String[] args) throws Exception {
+        run(GitMain.class, args);
+    }
+
+    @Override
+    protected void run(String[] args) throws Exception {
+        System.out.println();
+        System.out.println("Oozie Git Action Configuration");
+        LOG.debug("Oozie Git Action Configuration");
+        System.out
+                .println("=============================================");
+        LOG.debug("=============================================");
+        // loading action conf prepared by Oozie
+        Configuration actionConf = new Configuration(false);
+
+        String actionXml = System.getProperty("oozie.action.conf.xml");
+        if (actionXml == null) {
+            throw new RuntimeException(
+                    "Missing Java System Property [oozie.action.conf.xml]");
+        }
+        if (!new File(actionXml).exists()) {
+            throw new RuntimeException("Action Configuration XML file ["
+                    + actionXml + "] does not exist");
+        }
+
+        actionConf.addResource(new Path("file:///", actionXml));
+
+        try {
+            parseActionConfiguration(actionConf);
+        } catch (RuntimeException e) {
+            //convertException(e);
+            throw e;
+        }
+
+        File localKey = null;
+        if (keyPath != null) {
+          localKey = getKey(new Path(keyPath));
+        }
+
+        try {
+            cloneRepoToHdfs(new Path(destinationUri), new URI(gitUri), gitBranch, localKey);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    /**
+     * Gathers the Git authentication key from HDFS and copies it to a local
+     * filesystem location
+     *
+     * @param location where the key is located (an HDFS URI)
+     * @return the location to where the key was saved
+     */
+    private File getKey(Path location) throws IOException, URISyntaxException {
+        Configuration conf = new Configuration();
+        FileSystem hfs = FileSystem.newInstance(new URI(nameNode), conf);
+        File key = new File(Files.createTempDirectory(
+            Paths.get("."),
+            "keys_" + Long.toString(System.nanoTime()),
+            PosixFilePermissions
+                .asFileAttribute(PosixFilePermissions
+                   .fromString("rwx------")))
+            .toString());
+
+        System.out.println("Local mkdir called creating temp. dir at: " +
+            key.getAbsolutePath());
+        LOG.debug("Local mkdir called creating temp. dir at: " +
+            key.getAbsolutePath());
+
+        hfs.copyToLocalFile(location, new Path("file:///" +
+            key.getAbsolutePath() + "/privkey"));
+        System.out.println("Copied keys to local container!");
+        LOG.debug("Copied keys to local container!");
+        return(new File(key.getAbsolutePath() + "/privkey"));
+    }
+
+    /**
+     * Clones a Git repository
+     *
+     * @param gitSrc - URI to the Git repository being cloned
+     * @param branch - String for the Git branch (or null)
+     * @param outputDir - local file reference where the repository should be cloned
+     * @param credentialFile - local file path containing repository authentication key or null
+     */
+    private void cloneRepo(URI gitSrc, String branch, File outputDir, final File credentialFile) throws RuntimeException {
+        final SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+            @Override
+            protected void configure(OpenSshConfig.Host host, Session session) {
+
+            }
+
+            @Override
+            protected JSch createDefaultJSch(FS fs) throws JSchException {
+                JSch.setConfig("StrictHostKeyChecking", "no");
+                JSch defaultJSch = super.createDefaultJSch(fs);
+                if (credentialFile != null) {
+                    defaultJSch.addIdentity(credentialFile.toString());
+                }
+                return defaultJSch;
+            }
+        };
+
+        CloneCommand cloneCommand = Git.cloneRepository();
+        cloneCommand.setURI(gitSrc.toString());
+
+        if (gitSrc.getScheme().toLowerCase() == "ssh") {
+          cloneCommand.setTransportConfigCallback(new TransportConfigCallback() {
+              @Override
+              public void configure(Transport transport) {
+                  SshTransport sshTransport = (SshTransport)transport;
+                  sshTransport.setSshSessionFactory(sshSessionFactory);
+              }
+          });
+        }
+
+        cloneCommand.setDirectory(outputDir);
+        // set our branch identifier
+        if (branch != null) {
+            cloneCommand.setBranchesToClone(Arrays.asList("refs/heads/" + branch));
+        }
+
+        try {
+            cloneCommand.call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Was unable to clone Git repo: " + e);
+        }
+    }
+
+    /**
+     * Clone a repo up to HDFS a Git repository
+     *
+     * @param destination - HDFS path to which repository should be cloned
+     * @param gitSrc - Git repo URI to clone from
+     * @param branch - Git branch to clone
+     * @param credentialFile - local file path containing repository authentication key or null
+     * @throws Exception
+     */
+    private String cloneRepoToHdfs(Path destination, URI gitSrc, String branch, File credentialFile) throws Exception {
+        File tempD = new File(Files.createTempDirectory(
+            Paths.get("."),
+            "git_" + Long.toString(System.nanoTime()),
+            PosixFilePermissions
+                .asFileAttribute(PosixFilePermissions
+                   .fromString("rwx------")))
+            .toString());
+
+        System.out.println("Local mkdir called creating temp. dir at: " +
+            tempD.getAbsolutePath());
+        LOG.debug("Local mkdir called creating temp. dir at: " +
+            tempD.getAbsolutePath());
+
+        Configuration conf = new Configuration();
+        FileSystem hfs = FileSystem.get(destination.toUri(), conf);
+
+        cloneRepo(gitSrc, branch,
+            tempD, credentialFile);
+
+        // create a list of files and directories to upload
+        File src = new File(tempD.getAbsolutePath());
+        ArrayList<Path> srcs = new ArrayList<Path>(1000);
+        for (File p:src.listFiles()) {
+          srcs.add(new Path(p.toString()));
+        }
+
+        System.out.println("Finished cloning to local");
+        LOG.debug("Finished cloning to local");
+
+        hfs.mkdirs(destination);
+        hfs.copyFromLocalFile(false, true, srcs.toArray(new Path[0]), destination);
+        System.out.println("Finished the copy to " + destination.toString() + "!");
+        LOG.debug("Finished the copy to " + destination.toString() + "!");
+        return(destination.toString());
+    }
+
+    /**
+     * Parse action configuration and set configuration variables
+     *
+     * @param Oozie action configuration
+     * @throws RuntimeException upon any parse failure
+     */
+    private void parseActionConfiguration(Configuration actionConf) {
+        // APP_NAME
+        appName = actionConf.get(GitActionExecutor.APP_NAME);
+        if (appName == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.APP_NAME + " property");
+        }
+        //WORKFLOW_ID
+        workflowId = actionConf.get(GitActionExecutor.WORKFLOW_ID);
+        if (workflowId == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.WORKFLOW_ID + " property");
+        }
+        // CALLBACK_URL
+        callbackUrl = actionConf.get(GitActionExecutor.CALLBACK_URL);
+        if (callbackUrl == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.CALLBACK_URL + " property");
+        }
+        // JOB_TRACKER
+        jobTracker = actionConf.get(GitActionExecutor.JOB_TRACKER);
+        if (jobTracker == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.JOB_TRACKER + " property");
+        }
+        //NAME_NODE
+        nameNode = actionConf.get(GitActionExecutor.NAME_NODE);
+        if (nameNode == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.NAME_NODE + " property");
+        }
+        // DESTINATION_URI
+        destinationUri = actionConf.get(GitActionExecutor.DESTINATION_URI);
+        if (destinationUri == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.DESTINATION_URI + " property");
+        }
+        try {
+            FileSystem hfs = FileSystem.get(new URI(destinationUri), actionConf);
+            destinationUri = hfs.makeQualified(new Path(destinationUri)).toString();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + "a proper URI: " + destinationUri + " exception "
+                    + e.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + "a filesystem for URI " + GitActionExecutor.DESTINATION_URI + "exception "
+                    + e.toString());
+        }
+        // GIT_URI
+        gitUri = actionConf.get(GitActionExecutor.GIT_URI);
+        if (gitUri == null) {
+            throw new RuntimeException("Action Configuration has a null " +
+                    GitActionExecutor.GIT_URI + " property");
+        }
+        try {
+            if (new URI(gitUri).getScheme() == null) {
+              throw new RuntimeException("Action Configuration does not have "
+                      + "a proper URI " + gitUri);
+             }
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + "a proper URI " + gitUri + " exception "
+                    + e.toString());
+        }
+        // GIT_BRANCH
+        gitBranch = actionConf.get(GitActionExecutor.GIT_BRANCH);
+
+        // KEY_PATH
+        keyPath = actionConf.get(GitActionExecutor.KEY_PATH);
+        // ACTION_TYPE
+        actionType = actionConf.get(GitActionExecutor.ACTION_TYPE);
+        if (actionType == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.ACTION_TYPE + " property");
+        }
+        // ACTION_NAME
+        actionName = actionConf.get(GitActionExecutor.ACTION_NAME);
+        if (actionName == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.ACTION_NAME + " property");
+        }
+    }
+}
---
+++ sharelib/git/src/test/java/org/apache/oozie/action/hadoop/TestGitMain.java
@@ -0,0 +1,190 @@
+/** * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.oozie.action.hadoop;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.IOException;
+import java.lang.Class;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.JobID;
+import org.apache.hadoop.mapred.RunningJob;
+import org.apache.oozie.action.hadoop.ActionExecutorTestCase.Context;
+import org.apache.oozie.action.hadoop.LauncherMapperHelper;
+import org.apache.oozie.action.hadoop.SharelibUtils;
+import org.apache.oozie.client.WorkflowAction;
+import org.apache.oozie.command.wf.ActionXCommand.ActionExecutorContext;
+import org.apache.oozie.service.HadoopAccessorService;
+import org.apache.oozie.service.Services;
+import org.apache.oozie.service.WorkflowAppService;
+import org.apache.oozie.test.XTestCase.Predicate;
+import org.apache.oozie.util.IOUtils;
+import org.apache.oozie.util.XConfiguration;
+import org.apache.oozie.util.XmlUtils;
+import org.apache.oozie.WorkflowActionBean;
+import org.apache.oozie.WorkflowJobBean;
+import org.apache.oozie.WorkflowJobBean;
+import org.jdom.Element;
+
+import org.apache.oozie.test.XFsTestCase;
+
+public class TestGitMain extends XFsTestCase {
+
+    private static List<String> getAllFilePath(Path filePath, FileSystem fs) throws FileNotFoundException, IOException {
+        List<String> fileList = new ArrayList<String>();
+        FileStatus[] fileStatus = fs.listStatus(filePath);
+        for (FileStatus fileStat : fileStatus) {
+            if (fileStat.isDirectory()) {
+                fileList.addAll(getAllFilePath(fileStat.getPath(), fs));
+            } else {
+                fileList.add(fileStat.getPath().toString());
+            }
+        }
+        return fileList;
+    }
+
+    /*
+     * Test handling the key properly and that the file is secured
+     */
+    public void testKeyFile() throws Exception {
+
+/* XXX       GitActionExecutor ae = new GitActionExecutor();
+        assertTrue("Can not find GitMain class is launcher classes",
+          ae.getLauncherClasses().contains(GitMain.class)); */
+
+        final Path credentialFilePath = Path.mergePaths(getFsTestCaseDir(), new Path("/key_dir/my_key.dsa"));
+        final String credentialFileData = "Key file data";
+        final Path destDir = Path.mergePaths(getFsTestCaseDir(), new Path("/destDir"));
+        final String repoUrl = "https://github.com/apache/oozie";
+
+        // setup credential file data
+        FSDataOutputStream credentialFile = getFileSystem().create(credentialFilePath);
+        credentialFile.write(credentialFileData.getBytes());
+        credentialFile.flush();
+        credentialFile.close();
+
+/*        Element actionXml = XmlUtils.parseXml("<git>" +
+                "<job-tracker>" + getJobTrackerUri() + "</job-tracker>" +
+                "<name-node>" + getNameNodeUri() + "</name-node>" +
+                "<git-uri>" + repoUrl + "</git-uri>"+
+                "<key-path>" + credentialFilePath + "</key-path>"+
+                "<destination-uri>" + destDir + "</destination-uri>" +
+                "</git>");
+*/
+// XXX        XConfiguration protoConf = new XConfiguration();
+// XX        protoConf.set(WorkflowAppService.HADOOP_USER, getTestUser());
+
+// XXX        WorkflowJobBean wf = createBaseWorkflow(protoConf, "git-action");
+ //       WorkflowJobBean wf = new WorkflowJobBean();
+        WorkflowActionBean ga = new WorkflowActionBean();
+		ga.setType("git-action");
+ //       ActionExecutorContext context = new ActionExecutorContext(wf, ga);
+ //       WorkflowActionBean action = (WorkflowActionBean) wf.getActions().get(0);
+  //      action.setType(ae.getType());
+
+   //     Configuration conf = ae.createBaseHadoopConf(context, actionXml);
+
+        GitMain gitmain = new GitMain();
+
+        // allow us to call getKey()
+        Class<?>[] args = new Class<?>[] { Path.class } ;
+        Method getKeyMethod = gitmain.getClass().getDeclaredMethod("getKey", args);
+        getKeyMethod.setAccessible(true);
+ 
+        // Ugly hack to set namenode for getKeys()
+        Field field = gitmain.getClass().getDeclaredField("nameNode");
+        field.setAccessible(true);
+        field.set(gitmain, getNameNodeUri());
+
+        File localFile = (File) getKeyMethod.invoke(gitmain, credentialFilePath);
+//        File localFile = gitmain.getKey(credentialFilePath);
+
+        FileReader reader = new FileReader(localFile);
+        char[] testOutput = new char[credentialFileData.length()];
+        assertEquals(13, credentialFileData.length());
+        reader.read(testOutput, 0, credentialFileData.length());
+
+        assertEquals(credentialFileData, String.valueOf(testOutput));
+        reader.close();
+
+/*        Context context = createContext(actionXml);
+        final RunningJob launcherJob = submitAction(context);
+        String launcherId = context.getAction().getExternalId();
+//XXX        waitFor(120 * 1000, new Predicate() {
+        waitFor(30 * 1000, new Predicate() {
+            public boolean evaluate() throws Exception {
+                return launcherJob.isComplete();
+            }
+        });
+        assertTrue(launcherJob.isSuccessful());
+        Map<String, String> actionData = LauncherMapperHelper.getActionData(getFileSystem(), context.getActionDir(),
+                context.getProtoActionConf());
+        assertFalse(LauncherMapperHelper.hasIdSwap(actionData));
+
+        GitActionExecutor ae = new GitActionExecutor();
+        ae.check(context, context.getAction());
+        assertTrue(launcherId.equals(context.getAction().getExternalId()));
+        assertEquals("SUCCEEDED", context.getAction().getExternalStatus());
+        ae.end(context, context.getAction());
+        assertEquals(WorkflowAction.Status.OK, context.getAction().getStatus());
+
+*/
+
+//        assertTrue(getFileSystem().exists(outputPath));
+//        assertTrue(getFileSystem().exists(gitIndex));
+
+        // the Git conf index should easily fit in memory
+//        ByteArrayOutputStream readContent = new ByteArrayOutputStream();
+//        byte[] buffer = new byte[1024];
+//        InputStream is = getFileSystem().open(gitIndex);
+//        int length;
+//        while ((length = is.read(buffer)) != -1) {
+//            readContent.write(buffer, 0, length);
+//        }
+//        String gitIndexContent = readContent.toString("UTF-8");
+//        is.close();
+
+        // The Git index file should have a core, remote and branch section
+        // verify it was properly pulled down
+//        assertTrue(gitIndexContent.toLowerCase().contains("core"));
+//        assertTrue(gitIndexContent.toLowerCase().contains("remote"));
+//        assertTrue(gitIndexContent.toLowerCase().contains("branch"));
+    }
+
+}
