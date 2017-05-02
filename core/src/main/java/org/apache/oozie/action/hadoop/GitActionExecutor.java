77e8c84359da142e8f7dd125045dd541e04cec3b
Clay Baenziger <cbaenziger@bloomberg.net>
2017-05-01 21:37:20 -0400

    OOZIE-2877 Oozie Git Action

---
+++ core/src/main/java/org/apache/oozie/action/hadoop/GitActionExecutor.java
@@ -0,0 +1,179 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.oozie.action.hadoop;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.UnknownHostException;
+import java.util.List;
+
+import org.apache.hadoop.util.DiskChecker;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.AccessControlException;
+import org.apache.oozie.action.ActionExecutorException;
+import org.apache.oozie.action.hadoop.JavaActionExecutor;
+import org.apache.oozie.action.hadoop.LauncherMain;
+import org.apache.oozie.action.hadoop.LauncherMapper;
+import org.apache.oozie.action.hadoop.MapReduceMain;
+import org.apache.oozie.action.ActionExecutor;
+import org.apache.oozie.action.ActionExecutorException.ErrorType;
+import org.apache.oozie.action.ActionExecutorException;
+import org.apache.oozie.util.XmlUtils;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+
+public class GitActionExecutor extends JavaActionExecutor {
+
+    private static final String GIT_MAIN_CLASS_NAME =
+            "org.apache.oozie.action.hadoop.GitMain";
+    static final String APP_NAME = "oozie.oozie.app.name";
+    static final String WORKFLOW_ID = "oozie.oozie.workflow.id";
+    static final String CALLBACK_URL = "oozie.oozie.callback.url";
+    static final String JOB_TRACKER = "oozie.oozie.job.tracker";
+    static final String NAME_NODE = "oozie.oozie.name.node";
+    static final String GIT_URI = "oozie.git.git.uri";
+    static final String DESTINATION_URI = "oozie.git.destination.uri";
+    static final String KEY_PATH = "oozie.oozie.key.path";
+    static final String ACTION_TYPE = "oozie.oozie.action.type";
+    static final String ACTION_NAME = "oozie.oozie.action.name";
+
+    public GitActionExecutor() {
+        super("git");
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public List<Class> getLauncherClasses() {
+        List<Class> classes = super.getLauncherClasses();
+        classes.add(LauncherMain.class);
+        classes.add(MapReduceMain.class);
+        try {
+            classes.add(Class.forName(GIT_MAIN_CLASS_NAME));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Class not found", e);
+        }
+        return classes;
+    }
+
+    @Override
+    protected String getLauncherMain(Configuration launcherConf, Element actionXml) {
+        return launcherConf.get(LauncherMapper.CONF_OOZIE_ACTION_MAIN_CLASS,
+                GIT_MAIN_CLASS_NAME);
+    }
+
+    @Override
+    public void initActionType() {
+        super.initActionType();
+        registerError(UnknownHostException.class.getName(), ActionExecutorException.ErrorType.TRANSIENT, "HES001");
+        registerError(AccessControlException.class.getName(), ActionExecutorException.ErrorType.NON_TRANSIENT,
+                "JA002");
+        registerError(DiskChecker.DiskOutOfSpaceException.class.getName(),
+                ActionExecutorException.ErrorType.NON_TRANSIENT, "HES003");
+        registerError(org.apache.hadoop.hdfs.protocol.QuotaExceededException.class.getName(),
+                ActionExecutorException.ErrorType.NON_TRANSIENT, "HES004");
+        registerError(org.apache.hadoop.hdfs.server.namenode.SafeModeException.class.getName(),
+                ActionExecutorException.ErrorType.NON_TRANSIENT, "HES005");
+        registerError(ConnectException.class.getName(), ActionExecutorException.ErrorType.TRANSIENT, "  HES006");
+        registerError(JDOMException.class.getName(), ActionExecutorException.ErrorType.ERROR, "HES007");
+        registerError(FileNotFoundException.class.getName(), ActionExecutorException.ErrorType.ERROR, "HES008");
+        registerError(IOException.class.getName(), ActionExecutorException.ErrorType.TRANSIENT, "HES009");
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    Configuration setupActionConf(Configuration actionConf, Context context,
+                                  Element actionXml, Path appPath) throws ActionExecutorException {
+        super.setupActionConf(actionConf, context, actionXml, appPath);
+        Namespace ns = actionXml.getNamespace();
+
+        // APP_NAME
+        String appName = context.getWorkflow().getAppName();
+        if (appName == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.APP_NAME + " property");
+        }
+        //WORKFLOW_ID
+        String workflowId = context.getWorkflow().getId();
+        if (workflowId == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.WORKFLOW_ID + " property");
+        }
+        // CALLBACK_URL
+        String callbackUrl = context.getCallbackUrl("$jobStatus");
+        if (callbackUrl == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.CALLBACK_URL + " property");
+        }
+        actionConf.set(CALLBACK_URL, callbackUrl);
+
+        // JOB_TRACKER
+        Element jobTracker = actionXml.getChild("job-tracker", ns);
+        if (jobTracker == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.JOB_TRACKER + " property");
+        }
+        actionConf.set(JOB_TRACKER, jobTracker.getTextTrim());
+
+        //NAME_NODE
+        Element nameNode = actionXml.getChild("name-node", ns);
+        if (nameNode == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.NAME_NODE + " property");
+        }
+        actionConf.set(NAME_NODE, nameNode.getTextTrim());
+
+        // DESTINATION_URI
+        Element destinationUri = actionXml.getChild("destination-uri", ns);
+        if (destinationUri == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.DESTINATION_URI + " property");
+        }
+        actionConf.set(DESTINATION_URI, destinationUri.getTextTrim());
+
+        // GIT_URI
+        Element gitUri = actionXml.getChild("git-uri", ns);
+        if (gitUri == null) {
+            throw new RuntimeException("Action Configuration does not have "
+                    + GitActionExecutor.GIT_URI + " property");
+        }
+        actionConf.set(GIT_URI, gitUri.getTextTrim());
+                                                                        
+        Element keyPath = actionXml.getChild("ssh-key-path", ns);
+        if (keyPath != null) {
+          actionConf.set(KEY_PATH, keyPath.getTextTrim());
+        }
+
+        String actionType = getType();
+        String actionName = "git";
+
+        actionConf.set(APP_NAME, appName);
+        actionConf.set(WORKFLOW_ID, workflowId);
+        actionConf.set(ACTION_TYPE, actionType);
+        actionConf.set(ACTION_NAME, actionName);
+        return actionConf;
+    }
+
+    @Override
+    protected String getDefaultShareLibName(Element actionXml) {
+        return "git";
+    }
+}
---
+++ sharelib/git/src/test/java/org/apache/oozie/action/hadoop/TestGitActionExecutor.java
@@ -0,0 +1,212 @@
+/** * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.oozie.action.hadoop;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.JobID;
+import org.apache.hadoop.mapred.RunningJob;
+import org.apache.oozie.WorkflowActionBean;
+import org.apache.oozie.WorkflowJobBean;
+import org.apache.oozie.action.hadoop.ActionExecutorTestCase.Context;
+import org.apache.oozie.action.hadoop.LauncherMapperHelper;
+import org.apache.oozie.client.WorkflowAction;
+import org.apache.oozie.service.HadoopAccessorService;
+import org.apache.oozie.service.Services;
+import org.apache.oozie.action.hadoop.SharelibUtils;
+import org.apache.oozie.service.WorkflowAppService;
+import org.apache.oozie.test.XTestCase.Predicate;
+import org.apache.oozie.util.IOUtils;
+import org.apache.oozie.util.XConfiguration;
+import org.apache.oozie.util.XmlUtils;
+import org.jdom.Element;
+
+public class TestGitActionExecutor extends ActionExecutorTestCase{
+
+    @SuppressWarnings("unchecked")
+    public void testSetupMethods() throws Exception {
+        GitActionExecutor ae = new GitActionExecutor();
+        assertTrue("Can not find GitMain class is launcher classes",
+          ae.getLauncherClasses().contains(GitMain.class));
+
+        String repoUrl = "https://github.com/apache/oozie";
+        Element actionXml = XmlUtils.parseXml("<git>" +
+                "<job-tracker>" + getJobTrackerUri() + "</job-tracker>" +
+                "<name-node>" + getNameNodeUri() + "</name-node>" +
+                "<git-uri>" + repoUrl + "</git-uri>"+
+                "<destination-uri>repoDir</destination-uri>" +
+                "</git>");
+
+        XConfiguration protoConf = new XConfiguration();
+        protoConf.set(WorkflowAppService.HADOOP_USER, getTestUser());
+
+
+        WorkflowJobBean wf = createBaseWorkflow(protoConf, "git-action");
+        WorkflowActionBean action = (WorkflowActionBean) wf.getActions().get(0);
+        action.setType(ae.getType());
+
+        Context context = new Context(wf, action);
+
+        Configuration conf = ae.createBaseHadoopConf(context, actionXml);
+
+        String oozieActionGitRootLogger = "oozie.action.git" + LauncherMapper.ROOT_LOGGER_LEVEL;
+        conf.set(oozieActionGitRootLogger, "DEBUG");
+        ae.setRootLoggerLevel(conf);
+        assertEquals("DEBUG", conf.get(oozieActionGitRootLogger));
+
+        ae.setupActionConf(conf, context, actionXml, getFsTestCaseDir());
+        assertEquals(repoUrl, conf.get(GitActionExecutor.GIT_URI));
+        assertEquals("repoDir", conf.get(GitActionExecutor.DESTINATION_URI));
+    }
+
+    @Override
+    protected void setSystemProps() throws Exception {
+        super.setSystemProps();
+        setSystemProperty("oozie.service.ActionService.executor.classes", GitActionExecutor.class.getName());
+    }
+
+    private static List<String> getAllFilePath(Path filePath, FileSystem fs) throws FileNotFoundException, IOException {
+        List<String> fileList = new ArrayList<String>();
+        FileStatus[] fileStatus = fs.listStatus(filePath);
+        for (FileStatus fileStat : fileStatus) {
+            if (fileStat.isDirectory()) {
+                fileList.addAll(getAllFilePath(fileStat.getPath(), fs));
+            } else {
+                fileList.add(fileStat.getPath().toString());
+            }
+        }
+        return fileList;
+    }
+
+    /* 
+     * Test gitting a repo and ensure the data pulled down is a plausible Git repo to
+     * ensure that all works as expected
+     */
+    public void testGitFile() throws Exception {
+        final Path outputPath = getFsTestCaseDir();
+        final Path gitRepo = Path.mergePaths(outputPath, new Path("/repoDir"));
+        final Path gitIndex = Path.mergePaths(gitRepo, new Path("/.git/config"));
+
+        String repoUrl = "https://github.com/apache/oozie";
+        String actionXml = "<git>" +
+                "<job-tracker>" + getJobTrackerUri() + "</job-tracker>" +
+                "<name-node>" + getNameNodeUri() + "</name-node>" +
+                "<git-uri>" + repoUrl + "</git-uri>"+
+                "<destination-uri>" + gitRepo + "</destination-uri>" +
+                "</git>";
+
+        Context context = createContext(actionXml);
+        final RunningJob launcherJob = submitAction(context);
+        String launcherId = context.getAction().getExternalId();
+        waitFor(120 * 1000, new Predicate() {
+            public boolean evaluate() throws Exception {
+                return launcherJob.isComplete();
+            }
+        });
+        assertTrue(launcherJob.isSuccessful());
+        Map<String, String> actionData = LauncherMapperHelper.getActionData(getFileSystem(), context.getActionDir(),
+                context.getProtoActionConf());
+        assertFalse(LauncherMapperHelper.hasIdSwap(actionData));
+
+        GitActionExecutor ae = new GitActionExecutor();
+        ae.check(context, context.getAction());
+        assertTrue(launcherId.equals(context.getAction().getExternalId()));
+        assertEquals("SUCCEEDED", context.getAction().getExternalStatus());
+        ae.end(context, context.getAction());
+        assertEquals(WorkflowAction.Status.OK, context.getAction().getStatus());
+
+        assertTrue(getFileSystem().exists(outputPath));
+        assertTrue(getFileSystem().exists(gitIndex));
+
+        // the Git conf index should easily fit in memory
+        ByteArrayOutputStream readContent = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        InputStream is = getFileSystem().open(gitIndex);
+        int length;
+        while ((length = is.read(buffer)) != -1) {
+            readContent.write(buffer, 0, length);
+        }
+        String gitIndexContent = readContent.toString("UTF-8");
+        is.close();
+
+        // The Git index file should have a core, remote and branch section
+        // verify it was properly pulled down
+        assertTrue(gitIndexContent.toLowerCase().contains("core"));
+        assertTrue(gitIndexContent.toLowerCase().contains("remote"));
+        assertTrue(gitIndexContent.toLowerCase().contains("branch"));
+    }
+
+    private Context createContext(String actionXml) throws Exception {
+        GitActionExecutor ae = new GitActionExecutor();
+
+        XConfiguration protoConf = new XConfiguration();
+        protoConf.set(WorkflowAppService.HADOOP_USER, getTestUser());
+
+        FileSystem fs = getFileSystem();
+        SharelibUtils.addToDistributedCache("git", fs, getFsTestCaseDir(), protoConf);
+
+        WorkflowJobBean wf = createBaseWorkflow(protoConf, "git-action");
+        WorkflowActionBean action = (WorkflowActionBean) wf.getActions().get(0);
+        action.setType(ae.getType());
+        action.setConf(actionXml);
+
+        return new Context(wf, action);
+    }
+
+    protected RunningJob submitAction(Context context) throws Exception {
+        GitActionExecutor ae = new GitActionExecutor();
+
+        WorkflowAction action = context.getAction();
+
+        ae.prepareActionDir(getFileSystem(), context);
+        ae.submitLauncher(getFileSystem(), context, action);
+
+        String jobId = action.getExternalId();
+        String jobTracker = action.getTrackerUri();
+        String consoleUrl = action.getConsoleUrl();
+        assertNotNull(jobId);
+        assertNotNull(jobTracker);
+        assertNotNull(consoleUrl);
+
+        JobConf jobConf = Services.get().get(HadoopAccessorService.class).createJobConf(jobTracker);
+        jobConf.set("mapred.job.tracker", jobTracker);
+
+        JobClient jobClient =
+            Services.get().get(HadoopAccessorService.class).createJobClient(getTestUser(), jobConf);
+        final RunningJob runningJob = jobClient.getJob(JobID.forName(jobId));
+        assertNotNull(runningJob);
+        return runningJob;
+    }
+
+}
